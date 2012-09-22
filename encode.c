ea59418482fbaebc9b4f807b0a88fcefe699df67
Patrick Reagan <reaganpr@gmail.com>
2012-09-21 21:25:34 -0600

    Allows user-supplied offset for encoding

--- encode.c
+++ encode.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
 
-char shift_character(char source)
+char next_character(char source)
 {
-  char next;
+  char next = '\0';
+
   if(source == 'Z') {
     next = 'a';
   } else if(source == 'z') {
@@ -14,24 +16,61 @@ char shift_character(char source)
   } else {
     next = (char) source + 1;
   }
-  
+
   return next;
+}
+
+char shift_character(char source, int offset)
+{
+  int position = 0;
+  char shifted = source;
+
+  for(position = 0; position < offset; position++) {
+    shifted = next_character(shifted);
+  }
+
+  return shifted;
+}
+
+int character_between(char character, char lower_limit, char upper_limit)
+{
+  return character >= lower_limit && character <= upper_limit;
+}
+
+int can_shift(char source)
+{
+  return character_between(source, 'A', 'Z') || character_between(source, 'a', 'z');
 }
 
 int main(int argc, char *argv[])
 {
-  int i = 0;
-  
+  int i = 0, offset = 1;
+  char ch;
+
+  while((ch = getopt(argc, argv, "n:")) != -1) {
+    switch(ch) {
+      case 'n':
+        offset = atoi(optarg);
+        break;
+      default:
+        break;
+    }
+  }
+
   char source[BUFFER_SIZE];
   char *destination = strdup(source);
 
   while(fgets(source, BUFFER_SIZE, stdin)) {
     for(i = 0; source[i] != '\0'; i++) {
-      destination[i] = shift_character(source[i]);
+      if(can_shift(source[i])) {
+        destination[i] = shift_character(source[i], offset);
+      } else {
+        destination[i] = source[i];
+      }
     }
-  
+
     puts(destination);
   }
-  
+
   return 0;
 }
