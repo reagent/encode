e07c4c3e5d58df6c4753cec98a5f112bde83cc2b
Patrick Reagan <reaganpr@gmail.com>
2012-12-03 17:08:30 -0700

    Rewrite code to use APR

--- Makefile
+++ Makefile
@@ -1,6 +1,8 @@
-CFLAGS=-Wall -g
+PREFIX?=./vendor
+CFLAGS=-g -Wall -I${PREFIX}/apr/include/apr-1
+LDFLAGS=-L${PREFIX}/apr/lib -lapr-1 -pthread
 
 all: encode
 
 clean:
-	rm encode
+	rm -rf encode *.o *.dSYM
--- encode.c
+++ encode.c
@@ -1,112 +1,122 @@
+// C standard library includes
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-#define BUFFER_SIZE 1024
+// APR includes
+#include <apr_getopt.h>
+#include <apr_file_io.h>
+#include <apr_strings.h>
 
-void die(char *message)
+typedef char (*shift_cb)(char source);
+
+char
+next_character(char source)
 {
-  printf("Got error: '%s', exiting.\n", message);
-  exit(1);
-}
+    char next = '\0';
 
-char next_character(char source)
-{
-  char next = '\0';
-
-  if(source == 'Z') {
-    next = 'a';
-  } else if(source == 'z') {
-    next = 'A';
-  } else {
-    next = (char) source + 1;
-  }
-
-  return next;
-}
-
-char previous_character(char source)
-{
-  char previous = '\0';
-
-  if(source == 'a') {
-    previous = 'Z';
-  } else if(source == 'A') {
-    previous = 'z';
-  } else {
-    previous = (char) source - 1;
-  }
-
-  return previous;
-}
-
-int character_between(char character, char lower_limit, char upper_limit)
-{
-  return character >= lower_limit && character <= upper_limit;
-}
-
-int can_shift(char source)
-{
-  return character_between(source, 'A', 'Z') || character_between(source, 'a', 'z');
-}
-
-char shift_character(char source, int offset)
-{
-  int position = 0;
-  char shifted = source;
-
-  if(can_shift(source)) {
-    for(position = 0; position < abs(offset); position++) {
-      // TODO: use callbacks to supply shifting functions?
-      if(offset < 0) {
-        shifted = previous_character(shifted);
-      } else {
-        shifted = next_character(shifted);
-      }
+    if(source == 'Z') {
+        next = 'a';
+    } else if(source == 'z') {
+        next = 'A';
+    } else {
+        next = (char) source + 1;
     }
-  }
 
-  return shifted;
+    return next;
 }
 
-int main(int argc, char *argv[])
+char
+previous_character(char source)
 {
-  int offset = 1;
-  int should_increment = 1;
-  char ch = '\0';
-  char stream_source = 's';
-  FILE *input = stdin;
+    char previous = '\0';
 
-  while((ch = getopt(argc, argv, "dn:f:")) != -1) {
-    switch(ch) {
-      case 'd':
-        should_increment = 0;
-        break;
-      case 'n':
-        offset = abs(atoi(optarg));
-        break;
-      case 'f':
-        input = fopen(optarg, "r");
-        if(input == NULL) { die("Could not open input file"); }
-        stream_source = 'f';
-        break;
-      default:
-        break;
+    if(source == 'a') {
+        previous = 'Z';
+    } else if(source == 'A') {
+        previous = 'z';
+    } else {
+        previous = (char) source - 1;
     }
-  }
 
-  if(should_increment == 0) {
-    offset = -offset;
-  }
+    return previous;
+}
 
-  while((ch = fgetc(input)) != EOF) {
-    printf("%c", shift_character(ch, offset));
-  }
+int
+character_between(char character, char lower_limit, char upper_limit)
+{
+    return character >= lower_limit && character <= upper_limit;
+}
 
-  if(stream_source == 'f') {
-    fclose(input);
-  }
+int
+can_shift(char source)
+{
+    return character_between(source, 'A', 'Z') || character_between(source, 'a', 'z');
+}
 
-  return 0;
+char
+shift_character(char source, int offset)
+{
+    int position = 0;
+    char shifted = source;
+    shift_cb shift = (offset < 0) ? previous_character : next_character;
+
+    if(can_shift(source)) {
+        for(position = 0; position < abs(offset); position++) {
+            shifted = shift(shifted);
+        }
+    }
+
+    return shifted;
+}
+
+int
+main(int argc, const char *argv[])
+{
+    apr_pool_t *mp;
+    char next_char;
+
+    apr_initialize();
+
+    apr_pool_create(&mp, NULL);
+
+    apr_getopt_t *options;
+    char option;
+    const char *option_argument;
+
+    apr_int64_t shift_direction   = 1;
+    int shift_offset      = 1;
+
+    char *filename = NULL;
+
+    apr_getopt_init(&options, mp, argc, argv);
+    while (apr_getopt(options, "dn:f:", &option, &option_argument) == APR_SUCCESS) {
+        switch (option) {
+            case 'd':
+                shift_direction = -1;
+                break;
+            case 'n':
+                shift_offset = apr_atoi64(option_argument);
+                break;
+            case 'f':
+                filename = apr_pstrndup(mp, option_argument, 64);
+                break;
+        }
+    }
+
+    apr_file_t *source;
+
+    if (filename == NULL) {
+        apr_file_open_stdin(&source, mp);
+    } else {
+        apr_file_open(&source, filename, APR_FOPEN_READ, APR_FPROT_OS_DEFAULT, mp);
+    }
+
+    while (apr_file_getc(&next_char, source) == APR_SUCCESS) {
+        printf("%c", shift_character(next_char, shift_offset * shift_direction));
+    }
+
+    apr_pool_destroy(mp);
+    apr_terminate();
+
+    return 0;
 }
